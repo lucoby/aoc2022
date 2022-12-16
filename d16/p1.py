16c6d3d6e9f177289a1fca4e3f9d79f2514523f9
Coby Lu <coby.lu@appian.com>
2022-12-16 12:43:12 -0500

    d16 - inefficient

---
+++ d16/p1.py
@@ -0,0 +1,63 @@
+from dataclasses import dataclass
+from typing import Dict
+
+import networkx as nx
+
+from util import lines
+
+@dataclass
+class Valve:
+    name: str
+    flow_rate: int
+    distance: Dict = None
+
+    @staticmethod
+    def parse(l):
+        name = l[6:8]
+        flow_rate = int(l[23:].split(";")[0])
+        if len(l.strip().split("tunnels lead to valves ")) > 1:
+            tunnels = l.strip().split("tunnels lead to valves ")[1].split(", ")
+        else:
+            tunnels = l.strip().split("tunnel leads to valve ")[1].split(", ")
+        return Valve(name=name, flow_rate=flow_rate), tunnels
+
+
+def best_path(valves, opened, time, current, total_pressure):
+    if time <= 0:
+        return total_pressure
+    best_pressure = total_pressure
+    for v, valve in valves.items():
+        if v not in opened:
+            distance = valves[current].distance[v] + 1
+            pressure = (time - distance) * valve.flow_rate + total_pressure
+            new_opened = opened.copy()
+            new_opened.add(v)
+            final_pressure = best_path(valves, new_opened, time - distance, v, pressure)
+            best_pressure = max(best_pressure, final_pressure)
+    return best_pressure
+
+
+if __name__ == '__main__':
+    valves = {}
+    g = nx.Graph()
+    for l in lines("in1.txt"):
+        valve, tunnels = Valve.parse(l)
+        valves[valve.name] = valve
+        g.add_edges_from([(valve.name, t) for t in tunnels])
+
+    shortest_paths = dict(nx.all_pairs_shortest_path(g))
+
+    for s, target_path in shortest_paths.items():
+        distance = {t: len(path) - 1 for t, path in target_path.items()}
+        valves[s].distance = distance
+
+    valves = {v: valve for v, valve in valves.items() if v == "AA" or valve.flow_rate > 0}
+
+    time = 30
+    current = "AA"
+    opened = set()
+    total_pressure = best_path(valves, opened, 30, current, 0)
+    print(total_pressure)
+
+
+    print(valves)
